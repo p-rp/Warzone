815a536930548f95e77e0dc66ed7329e39d170b8
Piyush Pokharkar <66646406+p-rp@users.noreply.github.com>
2024-10-06 22:16:47 -0400

    Update GameEngineDriver.cpp

--- Warzone/GameEngineDriver.cpp
+++ Warzone/GameEngineDriver.cpp
@@ -1,8 +1,8 @@
-//#pragma once
-//#include "GameEngine.h"
-//
-//int main() {
-//	StateNode gameObj;
-//	//StateNode* currentNode = gameObj.initiliazeStates();
-//	gameObj.startupPhase();
-//}
+#pragma once
+#include "GameEngine.h"
+
+int main() {
+	StateNode gameObj;
+	//StateNode* currentNode = gameObj.initiliazeStates();
+	gameObj.startupPhase();
+}
